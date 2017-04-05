f3e2dfe5e320198d2d8b6d8d0c66ed19c97cb446
adityakpackt <adityak@packtpub.com>
2017-04-05 13:41:57 +0530

    Code files updated

---
+++ Chapter05/Win2016DevOps/OnlinePharmacy/Controllers/IndexController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlinePharmacy.Controllers
+{
+    public class IndexController : Controller
+    {
+        // GET: Index
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Chapter05/Win2016DevOps/OnlinePharmacy/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OnlinePharmacy.MvcApplication" Language="C#" %>
